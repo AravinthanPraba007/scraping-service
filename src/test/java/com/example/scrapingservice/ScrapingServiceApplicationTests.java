b36a6bce5b681dfd478d885d7aec38c424bc7b16
Aravinthan <aravinthanpro@gmail.com>
2021-11-08 09:59:56 +0530

    first spring boot commit

---
+++ src/test/java/com/example/scrapingservice/ScrapingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrapingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
